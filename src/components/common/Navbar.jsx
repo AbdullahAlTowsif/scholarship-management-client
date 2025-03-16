054d0d9bb298c54d0f0238782f009e70937485ff
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2025-03-17 02:36:49 +0600

    user pic showed to nav based on login

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,12 +1,14 @@
 import { Link, NavLink } from "react-router-dom";
-
+import useAuth from "../../hooks/useAuth";
+import user_pic from "../../assets/user.png"
 const Navbar = () => {
+    const { user, logOut } = useAuth()
     const links = (
         <>
             <li>
                 <NavLink
                     to={'/'}
-                    style={{backgroundColor: "#89890C", color: "white", fontWeight: "bold"}}
+                    style={{ backgroundColor: "#89890C", color: "white", fontWeight: "bold" }}
                 >
                     Home
                 </NavLink>
@@ -36,12 +38,45 @@ const Navbar = () => {
                     </ul>
                 </div>
                 <div className="navbar-end">
-                    <Link 
-                        to={'/auth/register'}
-                        className="btn btn-ghost font-bold text-white hover:bg-[#89890C]"
-                    >
-                        Register
-                    </Link>
+                    {
+                        !user && (
+                            <Link to="/auth/register" className="btn btn-ghost mr-2">
+                                Register
+                            </Link>
+                        )
+                    }
+
+                    {
+                        user && (
+                            <div className="dropdown dropdown-end">
+                                <div
+                                    tabIndex={0}
+                                    role="button"
+                                    className="btn btn-ghost btn-circle avatar"
+                                >
+                                    <div className="w-10 rounded-full">
+                                        <img alt="User Avatar" src={user && user.photoURL ? user.photoURL : user_pic} />
+                                    </div>
+                                </div>
+                                <ul
+                                    tabIndex={0}
+                                    className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+                                >
+                                    <li>
+                                        <span className="text-gray-700 font-semibold cursor-default">
+                                            {user.displayName}
+                                        </span>
+                                    </li>
+                                    <li>
+                                        <Link to="/dashboard/profile">Dashboard</Link>
+                                    </li>
+                                    <li>
+                                        <button onClick={logOut}>Logout</button>
+                                    </li>
+                                </ul>
+                            </div>
+                        )
+                    }
                 </div>
             </div>
         </div>
